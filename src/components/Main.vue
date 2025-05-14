74559b56f31107f3e71beffdc02bd702dd09414b
PickNucker <68569018+PickNucker@users.noreply.github.com>
2025-05-14 18:59:37 +0200

    callorie tracker project setup

---
+++ src/components/Main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container class="fill-height d-flex justify-center justify-space-between" max-width="800">
+    <h1>Hallo</h1>
+    <h1>Hallo</h1>
+  </v-container>
+</template>
