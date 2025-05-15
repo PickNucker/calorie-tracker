c0a12f150b2b4ef0a78899f96356997694678c22
PickNucker <68569018+PickNucker@users.noreply.github.com>
2025-05-15 21:01:53 +0200

    Menu Card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Chart from '@/components/ChardComponent.vue'
 import NavbarTracker from '@/components/NavbarTracker.vue'
 import MenuCards from '@/components/MenuCards.vue'
 </script>
@@ -8,8 +7,13 @@ import MenuCards from '@/components/MenuCards.vue'
   <NavbarTracker />
   <v-container class="fill-height d-flex justify-center justify-space-between" max-width="800">
     <h1>Hallo</h1>
-    <MenuCards />
-    <!-- <Chart/> -->
+    <div class="d-flex ga-5">
+
+      <MenuCards title="Breakfast" :carbs="0" :currentItems="0"/>
+      <MenuCards title="Lunch" :carbs="2" :currentItems="6"/>
+      <MenuCards title="Dinner" :carbs="4" :currentItems="10"/>
+    </div>
     <h1>Hallo</h1>
+    <!-- <Chart/> -->
   </v-container>
 </template>
--- src/components/MenuCards.vue
+++ src/components/MenuCards.vue
@@ -1,16 +1,22 @@
-<script>
+<script setup lang="ts">
+
+defineProps({
+  title: String,
+  carbs: Number,
+  currentItems: Number,
+})
 
 </script>
 
 
 <template>
-  <v-card class="w-25 h-25 rounded-xl">
-    <div class="d-flex ma-5 ga-2">
-      <v-icon icon="mdi-coffee" size="x-large"></v-icon>
+  <v-card class="flex rounded-xl w-100 ma-2 pr-8">
+    <div class="d-flex ma-5 ga-2 align-center">
+      <v-icon icon="mdi-coffee" class="pa-2 mr-10" style="font-size: 3.5em;"></v-icon>
       <div class="d-flex-col">
-        <h1>Breakfast</h1>
-        <h3>0 cals</h3>
-        <h3 color="blue">6 item</h3>
+        <h1>{{ title }}</h1>
+        <h3><strong>{{ carbs }} carbs</strong></h3>
+        <h3 style="color: aqua">{{ currentItems }} item</h3>
       </div>
     </div>
     <v-divider>
@@ -19,17 +25,26 @@
       </v-btn>
     </v-divider>
     <div class="d-flex-col ma-5 mr-10">
-      <div class="d-flex justify-space-between">
+      <div class="d-flex align-center justify-space-between pr-4">
         <v-icon icon="mdi-barley" size="x-large"></v-icon>
-        <h3>Carbs</h3>
+        <div class="flex align-start">
+          <h4>CARBS</h4>
+          <h4>0g</h4>
+        </div>
       </div>
-      <div class="d-flex justify-space-between">
+      <div class="d-flex align-center justify-space-between">
         <v-icon icon="mdi-fish" size="x-large"></v-icon>
-        <h3>Protein</h3>
+        <div class="flex align-start">
+          <h4>PROTEIN</h4>
+          <h4>0g</h4>
+        </div>
       </div>
-      <div class="d-flex justify-space-between">
+      <div class="d-flex align-center justify-space-between pr-10">
         <v-icon icon="mdi-water" size="x-large"></v-icon>
-        <h3>Fat</h3>
+        <div class="d-flex  flex-column">
+          <h4 class="text-uppercase">Fat</h4>
+          <h4>0g</h4>
+        </div>
       </div>
     </div>
   </v-card>
